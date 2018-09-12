3a13be4753bac7d8dc1f599ccae3896279004f2f
alan <alanwei1202@gmail.com>
2018-09-12 14:07:39 +0800

    add Event Handler

---
+++ DC_ezCommunication/Model/SerialPortModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DC_ezCommunication.Model
+{
+    class SerialPortModel
+    {
+        public delegate void SerialPortEventHandler(Object sender, SerialPortEventArgs e);
+    }
+
+    public class SerialPortEventArgs : EventArgs
+    {
+        public bool isOpend = false;
+        public Byte[] receiveBytes = null;
+    }
+}
